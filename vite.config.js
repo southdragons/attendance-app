07ea0b732cb466bb997717a00cc935c50846c1c8
kaki <takumi-kakimoto@tse.tamagawa-seiki.co.jp>
2026-03-25 15:46:56 +0900

    fix base path build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/', // ←これ追加
+  base:  '/attendance-app/',
   plugins: [vue()],
 })
