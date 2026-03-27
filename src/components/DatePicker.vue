29cb328e413892e7c4ee2d02888cd50780e9ce0f
kakimon <taku.umi721@gmail.com>
2026-03-27 22:33:42 +0900

    feat: UI改善 + UI修正2

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -4,48 +4,27 @@ import { ref } from 'vue'
 const props = defineProps(['date'])
 const emit = defineEmits(['change'])
 
-const dateInput = ref(null)
-
 function update(e) {
   emit('change', new Date(e.target.value))
-}
-
-function openPicker() {
-  dateInput.value.showPicker() // ←これが重要
 }
 </script>
 
 <template>
-  <!-- 👇 ここをタップ -->
-  <div class="card" @click="openPicker">
-    {{ props.date.toISOString().split('T')[0] }}
+  <div class="card">
+    <input
+      type="date"
+      :value="props.date.toISOString().split('T')[0]"
+      @input="update"
+      class="input"
+    />
   </div>
-
-  <!-- 👇 非表示のinput -->
-  <input
-    ref="dateInput"
-    type="date"
-    :value="props.date.toISOString().split('T')[0]"
-    @input="update"
-    class="hidden-input"
-  />
 </template>
 
 <style scoped>
-.card {
-  margin-bottom: 12px;
-  padding: 14px;
-  text-align: center;
-  font-size: 18px;
-  font-weight: bold;
-  background: #fff;
-  border-radius: 12px;
-}
-
-/* 完全非表示 */
-.hidden-input {
-  position: absolute;
-  opacity: 0;
-  pointer-events: none;
+.card { margin-bottom: 12px; }
+.input {
+  width: 100%;
+  height: 50px;
+  font-size: 16px;
 }
 </style>
